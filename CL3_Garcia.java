5b0890e4e7310ea2bbd62ee5d30091bd2230b60a
Saul Garcia <garcialuevanosaul@gmail.com>
2025-10-23 19:22:37 -0600

    CL3_Garcia.java

--- CL3_Garcia.java
+++ CL3_Garcia.java
@@ -1,15 +1,3 @@
-/* Your Full Name
-[CS1101] Comprehensive Lab 3
-This work is to be done individually. It is not permitted
-to. share, reproduce, or alter any part of this assignment
-for any purpose. Students are not permitted to share code,
-upload this assignment online in any form, or
-view/receive/ modifying code written by anyone else. This
-assignment is part of an academic course at The University
-of Texas at El Paso and a grade will be assigned for the
-work produced individually by the student.
-*/
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -376,6 +364,7 @@ public class CL3_Garcia{
 
         }
     }
+
 
 
 }
